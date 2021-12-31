7298cff91af9657f2203871c89b6dec509857972
Ant <it@me.org>
2021-12-31 16:41:28 -0600

    Working implementation of dapper.fsharp

--- fsharp-sql/Program.fs
+++ fsharp-sql/Program.fs
@@ -21,72 +21,73 @@
 //    for person in sql.
 //}
 
-//open System
-//open System.Data
-//open Dapper.FSharp
-//open Dapper.FSharp.PostgreSQL
-//open FSharp.Data.Sql.Providers
-//open Npgsql
-//
-//type Person =
-//    {
-//        Id : Guid
-//        Name : string
-//        Age : int
-//    }
-//
-//let personTable = table<Person>
-//
-//let conn = new NpgsqlConnection(@"Host=localhost;Database=TheNewPuppyPlace;Username=test;Password=test")
-//
-//let result = 
-//    select {
-//    for p in personTable do
-//        selectAll
-//    } |> conn.SelectAsync<Person> |> 
-//    
-//let newList = [1;2;3;4]
-//    
-//
-//result
-//|> Seq.map (fun x -> printfn "{x}")
-// 
-//newList
-////|> List.map (fun x -> string x)
-//|> List.map (fun x -> printfn "%i" x)
-
-(* *)
-
 open System
-open Npgsql.FSharp
+open System.Data
+open Dapper.FSharp
+open Dapper.FSharp.PostgreSQL
+open FSharp.Data.Sql.Providers
+open Npgsql
 
-let connectionString : string =
-    Sql.host "localhost"
-    |> Sql.database "fsharp"
-    |> Sql.username "test"
-    |> Sql.password "test"
-    |> Sql.port 5432
-    |> Sql.formatConnectionString
+type Person =
+    {
+        Id : int
+        Name : string
+    }
+
+let personTable = table'<Person> "persons" |> inSchema "public"
+
+let conn = new NpgsqlConnection(@"Host=localhost;Database=fsharp;Username=test;Password=test")
+
+let getEm = task {
     
-type Person = {
-    Id : int
-    Name : string
+    let! result = 
+        select {
+        for p in personTable do
+            selectAll
+        } |> conn.SelectAsync<Person>
+    
+ 
+    
+    result
+    |> Seq.toList
+    |> List.map (fun x -> printfn $"ID: {x.Id}\nName: {x.Name}")
+    |> ignore
 }
 
-let getAllUsers (connectionString: string)  =
-    connectionString
-    |> Sql.connect
-    |> Sql.query "SELECT * FROM persons"
-    |> Sql.execute (fun read ->
-        {
-            Id = read.int "Id"
-            Name = read.string "Name"
-        })
+getEm |> Async.AwaitTask |> Async.RunSynchronously
+ 
+(* *)
 
-let results = getAllUsers connectionString
-
-results
-|> List.map (fun x -> printfn $"Name: {x.Name}")
+//open System
+//open Npgsql.FSharp
+//
+//let connectionString : string =
+//    Sql.host "localhost"
+//    |> Sql.database "fsharp"
+//    |> Sql.username "test"
+//    |> Sql.password "test"
+//    |> Sql.port 5432
+//    |> Sql.formatConnectionString
+//    
+//type Person = {
+//    Id : int
+//    Name : string
+//}
+//
+//let getAllUsers (connectionString: string)  =
+//    connectionString
+//    |> Sql.connect
+//    |> Sql.query "SELECT * FROM persons"
+//    |> Sql.execute (fun read ->
+//        {
+//            Id = read.int "Id"
+//            Name = read.string "Name"
+//        })
+//
+//let results = getAllUsers connectionString
+//
+//results
+//|> List.map (fun x -> printfn $"Name: {x.Name}")
 //|> List.map (fun x -> printfn "%s" x)
     
 //getAllUsers
