392df7d9a3351b075c5e55cd29c2363ed77bc6e1
Ant <it@me.org>
2021-12-31 18:14:22 -0600

    Doesn't work. Just saving changes.

--- fsharp-sql/Program.fs
+++ fsharp-sql/Program.fs
@@ -6,21 +6,22 @@
 //let DbVendor = Common.DatabaseProviderTypes.POSTGRESQL
 //
 //[<Literal>]
-//let ConnString = "Host=localhost;Database=TheNewPuppyPlace;Username=test;Password=test"
+//let ConnString = "Host=localhost;Database=fsharp;Username=test;Password=test"
 //
 //[<Literal>]
-//let Schema = "TheNewPuppyPlace"
+//let Schema = "persons"
 //
 //let [<Literal>] resPath = @"/Users/anthony/.nuget/packages/npgsql/6.0.2"
 //
-////type DB = SqlDataProvider<DbVendor, ConnString, "", resPath, 1000, true, Owner="public, admin, references">
-//type DB = SqlDataProvider<Common.DatabaseProviderTypes.POSTGRESQL, "Host=localhost;Database=TheNewPuppyPlace;Username=test;Password=test">
-//let sql = DB.GetDataContext()
+//type DB = SqlDataProvider<DbVendor, ConnString, "", resPath, 1000, true, Owner="public">
+//let context = DB.GetDataContext()
 //
-//query {
-//    for person in sql.
-//}
+//let response =
+//    query {
+//        for p in context.
+//    }
 
+(* Dapper.Fsharp*)
 open System
 open System.Data
 open Dapper.FSharp
@@ -28,15 +29,61 @@ open Dapper.FSharp.PostgreSQL
 open FSharp.Data.Sql.Providers
 open Npgsql
 
+type PersonName = PersonName of string
+
 type Person =
     {
         Id : int
-        Name : string
+        Name : PersonName
     }
 
 let personTable = table'<Person> "persons" |> inSchema "public"
 
 let conn = new NpgsqlConnection(@"Host=localhost;Database=fsharp;Username=test;Password=test")
+
+Console.WriteLine("Please enter a new person's name:")
+let newPersonName = Console.ReadLine() 
+
+let makePersonName string =
+    match string with
+    | "" -> None
+    | _ -> Some (PersonName string)
+
+//let createPerson (personName:PersonName option)=
+//    match personName with
+//    | Some name -> 
+//        {
+//        Id = Random().Next(5, 100)
+//        Name = name
+//        }
+//        printfn "person created!"
+//    | None -> "e"
+
+let createPerson (name:PersonName option) =
+    match name with
+    | Some name ->
+        Some {
+            Id = Random().Next()
+            Name = name
+        }
+    | None -> None
+        
+type Error = Error
+
+let savePerson (person:Person option) : Result<Person, Error> =
+    match person with
+    | Some person -> task {
+        let! post =
+           insert {
+               into personTable
+               value person
+           }
+           |> conn.InsertAsync
+        printfn "Success!"
+        }
+    | None -> Error
+
+let vnewperson = newPersonName |> makePersonName |> createPerson |> savePerson |> Async.AwaitTask |> Async.RunSynchronously
 
 let getEm = task {
     
