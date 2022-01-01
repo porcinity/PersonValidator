5d348c8ebb3d45acf3a5365d145fb44ed97d2083
Ant <it@me.org>
2022-01-01 16:02:47 -0600

    getEm works

--- fsharp-sql/Program.fs
+++ fsharp-sql/Program.fs
@@ -1,78 +1,62 @@
-//#r "FSharp.Data.SQLProvider.dll"
-//open FSharp.Data.Sql
-//open Npgsql
-//
-//[<Literal>]
-//let DbVendor = Common.DatabaseProviderTypes.POSTGRESQL
-//
-//[<Literal>]
-//let ConnString = "Host=localhost;Database=fsharp;Username=test;Password=test"
-//
-//[<Literal>]
-//let Schema = "persons"
-//
-//let [<Literal>] resPath = @"/Users/anthony/.nuget/packages/npgsql/6.0.2"
-//
-//type DB = SqlDataProvider<DbVendor, ConnString, "", resPath, 1000, true, Owner="public">
-//let context = DB.GetDataContext()
-//
-//let response =
-//    query {
-//        for p in context.
-//    }
-
 (* Dapper.Fsharp*)
 open System
 open System.Data
 open Dapper.FSharp
 open Dapper.FSharp.PostgreSQL
 open FSharp.Data.Sql.Providers
+open Microsoft.FSharp.Core
 open Npgsql
 
 type PersonName = PersonName of string
+module PersonName =
+    let create s =
+        match s with
+        | "" -> None
+        | _ -> PersonName s |> Some
+        
+    let unwrap (Some (PersonName p)) = PersonName p
+    let value (PersonName p) = p
 
 type Person =
     {
         Id : int
         Name : PersonName
     }
-
-let personTable = table'<Person> "persons" |> inSchema "public"
-
-let conn = new NpgsqlConnection(@"Host=localhost;Database=fsharp;Username=test;Password=test")
-
-Console.WriteLine("Please enter a new person's name:")
-let newPersonName = Console.ReadLine() 
-
-let makePersonName string =
-    match string with
-    | "" -> None
-    | _ -> Some (PersonName string)
-
-//let createPerson (personName:PersonName option)=
-//    match personName with
-//    | Some name -> 
-//        {
-//        Id = Random().Next(5, 100)
-//        Name = name
-//        }
-//        printfn "person created!"
-//    | None -> "e"
-
-let createPerson (name:PersonName option) =
-    match name with
-    | Some name ->
-        Some {
-            Id = Random().Next()
+    
+type PersonDto  =
+    {
+        Id : int
+        Name : string
+    }
+module PersonDto =
+    let create (person:Person) =
+        let name = PersonName.value person.Name
+        {
+            Id = person.Id
             Name = name
         }
-    | None -> None
-        
-type Error = Error
+let personTable = table'<PersonDto> "persons" |> inSchema "public"
+let createPerson (name) =
+    {
+        Id = 9
+        Name = name
+    }
+let conn = new NpgsqlConnection(@"Host=localhost;Database=fsharp;Username=test;Password=test")
 
-let savePerson (person:Person option) : Result<Person, Error> =
-    match person with
-    | Some person -> task {
+//Console.WriteLine("Please enter a new person's name:")
+//let newPersonName = Console.ReadLine()
+
+//let name = PersonName.create newPersonName
+//
+//let test:PersonDto = {
+//    Id = 328142983
+//    Name = "Brain"
+//}
+
+//let saveme = PersonDto.create test
+
+let savePerson person =
+     task {
         let! post =
            insert {
                into personTable
@@ -81,9 +65,11 @@ let savePerson (person:Person option) : Result<Person, Error> =
            |> conn.InsertAsync
         printfn "Success!"
         }
-    | None -> Error
+     
+//test |> savePerson
 
-let vnewperson = newPersonName |> makePersonName |> createPerson |> savePerson |> Async.AwaitTask |> Async.RunSynchronously
+
+//let vnewperson = newPersonName |> makePersonName |> createPerson |> savePerson |> Async.AwaitTask |> Async.RunSynchronously
 
 let getEm = task {
     
@@ -91,7 +77,7 @@ let getEm = task {
         select {
         for p in personTable do
             selectAll
-        } |> conn.SelectAsync<Person>
+        } |> conn.SelectAsync<PersonDto>
     
  
     
